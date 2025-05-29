aa9e836afd7803f41ac41611a5d1c3360a334d35
Jay Yu <66248646+jayyu23@users.noreply.github.com>
2025-05-29 03:18:04 -0400

    minor

--- rust_exif_reader/target/debug/build/libc-635000b29f15f810/build_script_build-635000b29f15f810.d
+++
@@ -1,5 +0,0 @@
-/Users/jayyu/Documents/Jay/Work/Stanford/4-Senior/Spring/EE 292J/Final Project/starling-store/rust_exif_reader/target/debug/build/libc-635000b29f15f810/build_script_build-635000b29f15f810: /Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.172/build.rs
-
-/Users/jayyu/Documents/Jay/Work/Stanford/4-Senior/Spring/EE 292J/Final Project/starling-store/rust_exif_reader/target/debug/build/libc-635000b29f15f810/build_script_build-635000b29f15f810.d: /Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.172/build.rs
-
-/Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.172/build.rs:
--- rust_exif_reader/target/debug/deps/pkg_config-01703384c9b122ed.d
+++
@@ -1,7 +0,0 @@
-/Users/jayyu/Documents/Jay/Work/Stanford/4-Senior/Spring/EE 292J/Final Project/starling-store/rust_exif_reader/target/debug/deps/pkg_config-01703384c9b122ed.rmeta: /Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.32/src/lib.rs
-
-/Users/jayyu/Documents/Jay/Work/Stanford/4-Senior/Spring/EE 292J/Final Project/starling-store/rust_exif_reader/target/debug/deps/libpkg_config-01703384c9b122ed.rlib: /Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.32/src/lib.rs
-
-/Users/jayyu/Documents/Jay/Work/Stanford/4-Senior/Spring/EE 292J/Final Project/starling-store/rust_exif_reader/target/debug/deps/pkg_config-01703384c9b122ed.d: /Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.32/src/lib.rs
-
-/Users/jayyu/.cargo/registry/src/github.com-1ecc6299db9ec823/pkg-config-0.3.32/src/lib.rs:
